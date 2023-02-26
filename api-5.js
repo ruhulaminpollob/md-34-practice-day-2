b0f269908162759e4c7663cb9a12155786d8f53c
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-02-27 00:38:51 +0600

    random advice added

--- api-5.js
+++ api-5.js
@@ -1,6 +1,15 @@
 const loadAdvice=()=>{
     fetch('https://api.adviceslip.com/advice')
-    .then(res=>res.json)
-    .then(data=>console.log(data))
+    .then(res=>res.json())
+    .then(data=>displayAdvice(data))
 }
+const displayAdvice=(advice)=>{
+    console.log(advice.slip.id);
+    const adviceContainer=document.getElementById('advice-container')
+    adviceContainer.innerHTML=`
+    <h1 class="text-center text-lime-300 font-semibold">ADVICE # ${advice.slip.id}</h1>
+    <p class="font-bold text-white my-5">"${advice.slip.advice}"</p>
+    `
+}
+
 loadAdvice()
